e7255106d9b4a5e499ae1f2ef65b94dd096c0086
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-02-28 18:26:09 +0600

    Initial setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000
+const app = express()
+//const userRoute = require("./Routes/user.route")
+const dbconection = require("./config/dbconection")
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const dburl = process.env.DB_URL
+
+dbconection(dburl)
+
+app.use('/upload/image', express.static(__dirname + '/upload/image/'));
+
+app.get("/",(req,res)=>{
+    res.json({
+        "message":"hello"
+    })
+})
+app.use((err, req, res, next) => {
+    //console.error("error tushar",err.message);
+    res.status(500).json({ message: err.message });
+});
+
+app.listen(port,"192.168.10.13",() => {
+    console.log(`server running in ${port}`)
+    console.log("ok all right everything")
+})
