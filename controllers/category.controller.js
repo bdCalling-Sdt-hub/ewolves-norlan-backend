2a04439597cc161e61ddb70ba5be5b10f6272e00
Nadir Hossain <nadirhossain336@gmail.com>
2024-03-07 16:10:44 +0600

    created sub category added api

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -104,3 +104,102 @@ exports.updateCategory = async (req, res, next) => {
         next(error);
     }
 };
+
+
+
+
+exports.addSubCategory = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        console.log(id);
+        const category = await CategoryModel.findById(id);
+        if(!category){
+            return res.status(204).send({ status: 204, message: "No Data Found"});
+        }
+
+        const { name, color } = req.body;
+
+        let imageFileName = "";
+        if (req.files && req.files.image && req.files.image[0]) {
+            imageFileName = `/upload/image/${req.files.image[0].filename}`;
+        }
+
+        const value = { 
+            name: name,
+            color: color,
+            image: imageFileName
+        };
+
+        category.sub_category.push(value);
+        const newCategory = await category.save();
+
+        return res.status(200).send({ status: 200, message: "Sub Category Added Successfully", data: newCategory});
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+exports.getSubCategory = async (req, res, next) => {
+    try {
+        const category = await CategoryModel.find({});
+        if(!category){
+            return res.status(204).send({ status: 204, message: "No Data Found"});
+        }
+        return res.status(200).send({ status: 200, message: "Category fetch Successfully", data: category});
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.deleteSubCategory = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const category = await CategoryModel.findOne({_id: id});
+        if(!category){
+            return res.status(204).send({ status: 204, message: "No Data Found"});
+        }
+        return res.status(200).send({ status: 200, message: "Category fetch Successfully", data: category});
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+exports.updateSubCategory = async (req, res, next) => {
+    try {
+        const {id } = req.params;
+        const category= await CategoryModel.findOne({_id: id});
+        console.log(category);
+        if(!category){
+            return res.status(204).send({ status: 204, message: "No Data Found"});
+        }
+
+        const { name, primary_color, secondary_color } = req.body;
+        console.log(req.body);
+        let imageFileName = "";
+        if (req.files && req.files.image && req.files.image[0]) {
+            imageFileName = `/upload/image/${req.files.image[0].filename}`;
+        }
+
+        const fileName = category?.image?.split("/").pop();
+        const filePath = path.join(__dirname, '..', 'upload', 'image', fileName);
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+
+        category.name= name ? name : category.name,
+        category.primary_color= primary_color ? primary_color : category.primary_color,
+        category.secondary_color= secondary_color ? secondary_color : category.secondary_color,
+        category.image= imageFileName ? imageFileName : category.image
+        const result = await category.save();
+        
+        return res.status(200).send({ status: 200, message: "Category Updated Successfully", data: result});
+
+    } catch (error) {
+        next(error);
+    }
+};
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const categoryController = require("../controllers/category.controller.js")
 const configureFileUpload=require("../middlewares/fileUpload.middleware.js");
 
+// category
 router.post("/category", configureFileUpload(), categoryController.addCategory);
 router.get("/category", categoryController.getCategory);
 router.get("/category/:id", categoryController.getSingleCategory);
 router.delete("/delete-category/:id", categoryController.deleteCategory);
 router.patch("/update-category/:id", configureFileUpload(), categoryController.updateCategory);
+
+// sub category
+router.post("/sub-category/:id", configureFileUpload(), categoryController.addSubCategory);
+router.delete("/delete-sub-category/:id", categoryController.deleteSubCategory);
+router.patch("/update-sub-category/:id", configureFileUpload(), categoryController.updateSubCategory);
+
 module.exports = router;
