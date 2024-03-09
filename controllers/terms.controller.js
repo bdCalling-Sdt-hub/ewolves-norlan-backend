e25e3aa069efdb4d1396c26ecab7b22395230185
Nadir Hossain <nadirhossain336@gmail.com>
2024-03-09 12:48:58 +0600

    terms and condition api created successfully

--- controllers/terms.controller.js
+++ controllers/terms.controller.js
@@ -16,3 +16,35 @@ exports.addTerms= async(req, res, next)=>{
         next(error)
     }
 }
+
+exports.getTerms = async(req, res, next)=>{
+    try {
+        const terms = await TermsModel.find({});
+        if(!terms){
+            return sendResponse(res, 204, "No Data Found", terms);
+        }
+
+        return sendResponse(res, 200, "Terms And Condition Fetch Data Successfully", terms);
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.updateTerms = async(req, res, next)=>{
+    try {
+        const {id} = req.params;
+        const {name, description} = req.body;
+        const terms = await TermsModel.findById(id);
+        if(!terms){
+            return sendResponse(res, 204, "No Data Found", terms);
+        }
+
+        terms.name = name ? name : terms.name;
+        terms.description=  description ? description : terms.description;
+        const result = await terms.save();
+        return sendResponse(res, 200, "Terms and Condition Updated Successfully", result);
+
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/terms.routes.js
+++ routes/terms.routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const termsController = require("../controllers/terms.controller");
 const configureFileUpload = require("../middlewares/fileUpload.middleware");
 router.post("/create-terms-and-condition", configureFileUpload(), termsController.addTerms);
+router.get("/get-terms-and-condition", termsController.getTerms);
+router.patch("/update-terms-and-condition/:id", configureFileUpload(), termsController.updateTerms);
 module.exports = router
