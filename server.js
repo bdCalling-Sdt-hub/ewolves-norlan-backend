bc18de2999dfd53e14ba3a102079795c8f184910
siffahim <siffahim25@gmail.com>
2024-03-09 11:58:39 +0600

    :feat: gig created

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000
-const app = express()
-const userRoute = require("./routes/user.route")
-const dbconection = require("./config/dbconection")
+const port = process.env.PORT || 5000;
+const app = express();
+const userRoute = require("./routes/user.route");
+const dbconection = require("./config/dbconection");
+const gigRouter = require("./routes/gig.route");
+const bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const dburl = process.env.DB_URL
+const dburl = process.env.DB_URL;
 
-dbconection(dburl)
+dbconection(dburl);
 
 app.use("/api/auth/", userRoute);
+app.use("/api/gig/", gigRouter);
 
+//image get
+app.use(express.static("uploads"));
 
-app.use('/upload/image', express.static(__dirname + '/upload/image/'));
-
-app.get("/",(req,res)=>{
-    res.json({
-        "message":"hello"
-    })
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello",
+  });
 });
-
-
 
 app.use((err, req, res, next) => {
-    //console.error("error tushar",err.message);
-    res.status(500).json({ message: err.message });
+  //console.error("error tushar",err.message);
+  res.status(500).json({ message: err.message });
 });
 
-
-
-const server = app.listen(port,"192.168.10.13",() => {
-    console.log(`server running in ${port}`)
-    console.log("ok all right everything")
+const server = app.listen(port, "192.168.10.16", () => {
+  console.log(`server running in ${port}`);
+  console.log("ok all right everything");
 });
 
 const io = require("socket.io")(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: "http://localhost:5173",
-        // credentials: true,
-    },
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+    // credentials: true,
+  },
 });
 
-
 // Socket.IO
-io.on('connection', (socket) => {
-    console.log(`Socket ${socket.id} connected`);
+io.on("connection", (socket) => {
+  console.log(`Socket ${socket.id} connected`);
 });
