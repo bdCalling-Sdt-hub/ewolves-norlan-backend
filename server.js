20111c9c9757e9d1889c60f3b71ae900323bf467
siffahim <siffahim25@gmail.com>
2024-06-13 16:24:35 +0600

    :rocket: ready for final deploy

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const port = process.env.PORT || 5000;
-const dbConnection = require("./config/db");
 const socketIo = require("socket.io");
 const socketHandler = require("./helper/socketHelper");
 const color = require("colors");
 const app = require("./app");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
 //db connect here
-dbConnection(process.env.DB_URL);
-const server = app.listen(port, "104.248.15.129", () => {
-  console.log(color.yellow("Application running on port", port));
-});
+async function main() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log(color.green("♻️  Database connected successfully"));
 
-//socket listen here
-// const io = socketIo(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "*",
-//   },
-// });
+    const server = app.listen(port, "104.248.15.129", () => {
+      console.log(color.yellow("🚀 Application running on port", port));
+    });
 
-// socketHandler(io);
-// global.io = io;
+    //socket listen here
+    // const io = socketIo(server, {
+    //   pingTimeout: 60000,
+    //   cors: {
+    //     origin: "*",
+    //   },
+    // });
+
+    // socketHandler(io);
+    // global.io = io;
+  } catch (error) {
+    console.log("🤢 Failed to connect database", error);
+  }
+}
+
+main();
